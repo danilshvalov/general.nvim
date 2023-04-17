6c5da49f2747a51e585cfebeb7ecc427067bd9e6
Daniil Shvalov <shvalovich@gmail.com>
2023-04-17 15:05:53 +0300

    feat: make `new` optional

--- lua/keymap/init.lua
+++ lua/keymap/init.lua
@@ -26,15 +26,23 @@ M.setup = function(config)
   M.config = vim.tbl_deep_extend("force", default_config, config or {})
 end
 
-M.map = {}
+Map = {}
 
-function M.map:new(opts)
+function Map:new(opts)
   local this = {}
   opts = vim.tbl_deep_extend("force", M.config.default_opts, opts or {})
 
-  self:prefix(opts.prefix or "", opts.label)
-  self:mode(opts.mode or "n")
-  self:ft(opts.ft)
+  this.__prefix = opts.prefix or ""
+  this.__label = opts.label
+  this.__mode = parse_mode(opts.mode)
+  this.__ft = opts.ft
+
+  if this.__prefix and this.__label then
+    local ok, wk = pcall(require, "which-key")
+    if ok then
+      wk.register({ name = this.__label }, { prefix = this.__prefix, mode = self.__mode })
+    end
+  end
 
   opts.ft = nil
   opts.prefix = nil
@@ -42,15 +50,13 @@ function M.map:new(opts)
   opts.label = nil
   this.__opts = opts
 
-  this.__mappings = {}
-
   setmetatable(this, self)
   self.__index = self
 
   return this
 end
 
-function M.map:set(lhs, rhs, opts)
+function Map:set(lhs, rhs, opts)
   local prefix = self.__prefix or ""
   opts = vim.tbl_deep_extend("force", self.__opts, opts or {})
 
@@ -70,31 +76,39 @@ function M.map:set(lhs, rhs, opts)
   return self
 end
 
-function M.map:prefix(prefix, label)
-  self.__prefix = prefix
-
-  if prefix and label then
-    local ok, wk = pcall(require, "which-key")
-    if ok then
-      wk.register({ name = label }, { prefix = prefix, mode = self.__mode })
-    end
-  end
-
-  return self
+function Map:prefix(prefix, label)
+  return Map:new(vim.tbl_deep_extend("force", self.__opts, {
+    prefix = prefix,
+    label = label,
+    mode = self.__mode,
+    ft = self.__ft,
+  }))
 end
 
-function M.map:mode(mode)
-  self.__mode = parse_mode(mode)
-  return self
+function Map:mode(mode)
+  return Map:new(vim.tbl_deep_extend("force", self.__opts, {
+    prefix = self.__prefix,
+    label = self.__label,
+    mode = mode,
+    ft = self.__ft,
+  }))
 end
 
-function M.map:ft(filetype)
-  if filetype == "all" then
-    self.__ft = nil
-  else
-    self.__ft = filetype
-  end
-  return self
+function Map:ft(ft)
+  return Map:new(vim.tbl_deep_extend("force", self.__opts, {
+    prefix = self.__prefix,
+    label = self.__label,
+    mode = self.__mode,
+    ft = ft,
+  }))
 end
+
+M.map = setmetatable({}, {
+  __index = function(_, key)
+    return Map:new()[key]
+  end,
+})
+
+M.setup()
 
 return M
